90cdce1aa27dcccc40ed0222d7b68d7552f3064f
Asmita Das <Asmita.Das@gds.ey.com>
2020-05-08 20:23:56 +0530

    controller updated

--- application/src/main/java/com/demo/springboot/controller/HelloController.java
+++ application/src/main/java/com/demo/springboot/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.demo.springboot.application;
+package com.demo.springboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
